cabe2c3a3f63f98827843bb677ffa45e1f1a957f
fathysamir <01154857080fa@gmail.com>
2024-08-05 17:31:26 +0300

    Initial commit

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -37,18 +37,54 @@ class RolesSeeder extends Seeder
         $permissions = Permission::pluck('id', 'id')->all();
 
         $admin_role->syncPermissions($permissions);
-        $user = User::create([
-            'first_name' => 'Admin',
-            'last_name' => 'Admin',
+        $user1 = User::create([
+            'first_name' => 'Admin1',
+            'last_name' => 'Admin1',
             'email' => 'monzer@gmail.com',
             'username'=>'monzer',
             'phone' => null,
-            'password' => Hash::make('gmadmin159!48@26#0'),
+            'password' => Hash::make('gmadmin159!48@26#1'),
+        ]);
+        $user2 = User::create([
+            'first_name' => 'Admin2',
+            'last_name' => 'Admin2',
+            'email' => 'admin2@gmail.com',
+            'username'=>'admin2',
+            'phone' => null,
+            'password' => Hash::make('gmadmin159!48@26#2'),
+        ]);
+        $user3 = User::create([
+            'first_name' => 'Admin3',
+            'last_name' => 'Admin3',
+            'email' => 'admin3@gmail.com',
+            'username'=>'admin3',
+            'phone' => null,
+            'password' => Hash::make('gmadmin159!48@26#3'),
+        ]);
+        $user4 = User::create([
+            'first_name' => 'Admin4',
+            'last_name' => 'Admin4',
+            'email' => 'admin4@gmail.com',
+            'username'=>'admin4',
+            'phone' => null,
+            'password' => Hash::make('gmadmin159!48@26#4'),
+        ]);
+        $user5 = User::create([
+            'first_name' => 'Admin5',
+            'last_name' => 'Admin5',
+            'email' => 'admin5@gmail.com',
+            'username'=>'admin5',
+            'phone' => null,
+            'password' => Hash::make('gmadmin159!48@26#5'),
         ]);
 
         
 
-        $user->assignRole([$admin_role->id]);
+        $user1->assignRole([$admin_role->id]);
+        $user2->assignRole([$admin_role->id]);
+        $user3->assignRole([$admin_role->id]);
+        $user4->assignRole([$admin_role->id]);
+        $user5->assignRole([$admin_role->id]);
 
         
     }
